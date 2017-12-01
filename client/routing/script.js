98dabf5b9c0ca2c1bc3ab8007c6f6012f8ad76a3
tedhagos <tedhagos@gmail.com>
2017-12-01 10:35:52 +0800

    routing

---
+++ client/routing/script.js
@@ -0,0 +1,24 @@
+let app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider){
+  $locationProvider.hashPrefix('');
+  $routeProvider
+    .when('/one', {
+      templateUrl: "one.html",
+      controller: "one"
+    })
+    .when('/two' ,{
+      templateUrl: "two.html"
+    })
+    .when('/three' ,{
+      templateUrl: "three.html"
+    })
+    .when('/four' ,{
+      templateUrl: "four.html"
+    })
+    .otherwise({redirectTo: '/'})
+});
+
+app.controller('one', function() {
+  
+})
